5eadf6955f0f45f99c15ce1d87390db9cd8ea7b8
AlexRioDe <voropaev.company@gmail.com>
2026-02-19 13:47:49 +0300

    home page

--- src/js/components/stories-slider.js
+++ src/js/components/stories-slider.js
@@ -10,28 +10,19 @@ export function storiesSlider() {
             slider['splide'] = new Splide(slider, {
                 fixedWidth: 112,
                 perMove: 1,
-                perPage: 10,
+                omitEnd: true,
                 gap: 16,
                 pagination: false,
                 breakpoints: {
                     1440: {
                         gap: 24,
-                        perPage: 9,
-                    },
-                    1224: {
-                        perPage: 7,
                     },
                     1024: {
                         gap: 16,
-                        perPage: 6,
                     },
                     768: {
-                        pagination: false,
                         arrows: false,
-                    },
-                    584: {
-                        perPage: 3,
-                    },
+                    }
                 },
 
                 arrowPath: 'M16.204 12.396a1 1 0 011.4-.192l5.618 4.267a4.391 4.391 0 010 7.058l-5.617 4.267a1 1 0 11-1.21-1.592l5.617-4.268c1.317-1 1.317-2.872 0-3.872l-5.616-4.268a1 1 0 01-.192-1.4z'
